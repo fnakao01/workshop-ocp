d5dbbc5932299d2283ea3bb9ff3b7c7a501874ce
fnakao01 <54185184+fnakao01@users.noreply.github.com>
2019-08-16 16:20:20 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
  node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop-ocp', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'worksho-ocp', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'workshop-ocp')
+        openshiftDeploy(deploymentConfig: 'worksho-ocp')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
